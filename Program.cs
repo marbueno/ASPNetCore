a22b416bf86e870f32d0db39f3b1384e6354c1f9
marbueno <marbue@ig.com.br>
2018-07-22 11:49:35 -0300

    Adicionado novo projeto Curso de AspNetCore

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ASPNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webHost = new WebHostBuilder()
+            .UseKestrel()
+            .Configure(
+                app => {
+                    app.Run(context => context.Response.WriteAsync("Hello World!"));
+                }).Build();
+
+            webHost.Run();
+        }
+    }
+}
